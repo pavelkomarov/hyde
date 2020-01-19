51f88256dd5ddf696c23be22c28925738e1c74f4
Pavel Komarov <pvlkmrv@gmail.com>
2020-01-18 20:53:54 -0500

    think I fixed the navbar javascript to address #5. Also I think the way I implemented page-scroll is going to make solving #2 super easy

--- assets/agency.js
+++ assets/agency.js
@@ -1,48 +1,41 @@
 
-// Set the action of the navbar buttons. jQuery for page page-scrolling feature
-// (requires jQuery Easing plugin); snap to correct url if not on home page.
-$('a.page-scroll').click(function(event) {
-	event.preventDefault();
-	let link = $(this).attr('href');
-	let site_root = window.location.protocol + '//' + window.location.host + '/';
-	
-	// match if we're currently on the home page
-	if (window.location.href.match('^https?://.*/(#.*)?$')) {
-		$('html, body').stop().animate({
-			scrollTop: $(link).offset().top
-		}, 1500, 'easeInOutExpo');
-		setTimeout(() => {window.location = site_root + link}, 1500);
-	} else {
-		window.location = site_root + link
-	}
-});
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({ target: '.navbar', offset: 200 })
+// Smooth scrolling for page-scroll elements
+for (let link of document.querySelectorAll('a.page-scroll')) {
+	link.addEventListener('click', (event) => {
+		let where_to = event.target.href.split('#');
+		if (where_to[0] == window.location) { // then we're on the same page, so scroll smoothly
+			event.preventDefault();
+			anchor = document.querySelector('#'+where_to[1]);
+			anchor.scrollIntoView({ behavior: 'smooth' }); // smooth, baby
+		}
+	});
+}
 
 // A function to fade-in the navbar's background on the homepage
 // based on vertical location
 function navbar_opacity() {
-	let offset = $(document).scrollTop();
-	let opacity = 1; // by default the background appears
-	if (window.location.href.match('^https?://.*/(#.*)?$')) {
-		if (offset <= 200) {
-			opacity = 0;
-		} else if (200 <= 500){
-			opacity = (offset - 200)/300;
-		}
+	let offset = window.scrollY;
+	let opacity = 1
+	if (offset <= 200) {
+		opacity = 0;
+	} else if (200 <= 500){
+		opacity = (offset - 200)/300;
 	}
-	$('.navbar').css('background-color', 'rgba(34,34,34,'+opacity+')');
+	document.querySelector('nav').style.backgroundColor = 'rgba(34,34,34,'+opacity+')';
 }
-window.addEventListener('scroll', navbar_opacity);// avoid jQuery where easy
-window.addEventListener('load', navbar_opacity);
-
-console.log("yeah, it gets run");
+if (window.location.href.match('^https?://.*/(#.*)?$')) { // if on homepage
+	window.addEventListener('scroll', navbar_opacity);
+	//window.addEventListener('load', navbar_opacity);
+} else { // on not-homepages, navbar is never transparent
+	document.querySelector('nav').style.backgroundColor = 'rgba(34,34,34,1)';
+}
 
 // Closes the Responsive Menu on Menu Item Click
-for (let link of document.querySelectorAll('.navbar-collapse ul a')) {
-	link.click(() => {
-		console.log('clicked me');
+for (let link of document.querySelectorAll('.navbar-collapse a')) {
+	link.addEventListener('click', () => {
 		document.getElementById('hamburger').click();
 	});
 }
+
+
+
