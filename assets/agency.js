0b3a60b2bf8126a7870b6da3960dac63808d2db3
Pavel Komarov <pvlkmrv@gmail.com>
2020-01-18 23:03:42 -0500

    moved some of the js around and just call the opacity function now instead of putting it in an event listener for page load; the js is already running at page load. css that was really meant for nav-links was labeled for page-scrolls, so fixed that. Tested using page-scroll for links on quotes page, and they seem to scroll right

--- assets/agency.js
+++ assets/agency.js
@@ -1,17 +1,4 @@
 
-// Smooth scrolling for page-scroll elements
-for (let link of document.querySelectorAll('a.page-scroll')) {
-	link.addEventListener('click', (event) => {
-		let where_to = event.target.href.split('#');
-		let where_is = window.location.href.split('#');
-		if (where_to[0] == where_is[0]) { // then we're on the same page, so scroll smoothly
-			event.preventDefault();
-			anchor = document.querySelector('#'+where_to[1]);
-			anchor.scrollIntoView({ behavior: 'smooth' }); // smooth, baby
-		}
-	});
-}
-
 // A function to fade-in the navbar's background on the homepage
 // based on vertical location
 function navbar_opacity() {
@@ -25,8 +12,8 @@ function navbar_opacity() {
 	document.querySelector('nav').style.backgroundColor = 'rgba(34,34,34,'+opacity+')';
 }
 if (window.location.href.match('^https?://[^/]+/(#.*)?$')) { // if on homepage
+	navbar_opacity(); // so proper transparency when linked back to #section
 	window.addEventListener('scroll', navbar_opacity);
-	window.addEventListener('load', navbar_opacity); // so proper transparency when linked back to #section
 } else { // on not-homepages, navbar is never transparent
 	document.querySelector('nav').style.backgroundColor = 'rgba(34,34,34,1)';
 }
@@ -41,5 +28,16 @@ for (let link of document.querySelectorAll('.navbar-collapse a')) {
 	});
 }
 
-
+// Smooth scrolling for page-scroll elements
+for (let link of document.querySelectorAll('a.page-scroll')) {
+	link.addEventListener('click', event => {
+		let where_to = event.target.href.split('#');
+		let where_is = window.location.href.split('#');
+		if (where_to[0] == where_is[0]) { // then we're on the same page, so scroll smoothly
+			event.preventDefault();
+			anchor = document.querySelector('#'+where_to[1]);
+			anchor.scrollIntoView({ behavior: 'smooth' }); // smooth, baby
+		}
+	});
+}
 
